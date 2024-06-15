e46eb69cbdab97566b78d4815f334199d5e9afd2
weixun <68503528+weixuna@users.noreply.github.com>
2024-06-15 03:43:33 +0000

    修改

--- HwIoTData/HwIoTData/settings.py
+++ HwIoTData/HwIoTData/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x%n^kwa^soi0f-qnoipycs)m-)ax6dvvzdlr9ohwzl(o_ccl#i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
